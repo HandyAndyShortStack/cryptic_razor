cfd55ae6ca2afd55f79f0e7d80e7e347a66e7f18
handyandyshortstack <andynshort@gmail.com>
2014-05-12 09:58:41 -0700

    removed testing reversal

--- spec/api/json_api_spec.rb
+++ spec/api/json_api_spec.rb
@@ -9,7 +9,7 @@ models.each do |model|
         instance = model.new
         json = load_json model.to_s.underscore.to_sym
 
-        expect(instance.to_json_api).not_to eq(json)
+        expect(instance.to_json_api).to eq(json)
       end
     end
   end
